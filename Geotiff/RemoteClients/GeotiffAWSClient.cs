f1071051a7b20d4b6dab9df2a770ea613acf42c5
oshawa-connection <jf13g13@gmail.com>
2025-10-04 15:00:41 +0100

    First pass AWS client

--- Geotiff/RemoteClients/GeotiffAWSClient.cs
+++ Geotiff/RemoteClients/GeotiffAWSClient.cs
@@ -9,24 +9,39 @@ using System.IO;
 using System.Threading.Tasks;
 
 
-public class GeotiffAWSClient
+public class GeotiffAWSClient :IGeotiffRemoteClient
 {
-    public async Task DownloadRangeFromS3Async(string bucketName, string key, long start, long end)
+    private string bucketName { get; set; }
+    private string key{ get; set; }
+    private AmazonS3Client amazonS3Client { get; set; }
+    public GeotiffAWSClient(string bucketName, string key, AmazonS3Client amazonS3Client)
     {
-        using var client = new AmazonS3Client();
+        this.bucketName = bucketName;
+        this.key = key;
+        this.amazonS3Client = amazonS3Client;
+    }
+    private async Task<ArrayBuffer> DownloadRangeFromS3Async(long start, long end, CancellationToken? signal = null)
+    {
         var request = new GetObjectRequest
         {
             BucketName = bucketName,
             Key = key,
             ByteRange = new ByteRange(start, end) // inclusive range,
-            
         };
 
-        using var response = await client.GetObjectAsync(request);
-        using var stream = response.ResponseStream;
-        using var ms = new MemoryStream();
-        await stream.CopyToAsync(ms);
-        byte[] data = ms.ToArray();
-        // new ArrayBuffer()
+        using var response = await this.amazonS3Client.GetObjectAsync(request);
+        await using var stream = response.ResponseStream;
+        return await ArrayBuffer.FromStream(stream, signal);
+    }
+
+    public async Task<IEnumerable<ArrayBuffer>> FetchSlices(IEnumerable<Slice> slices, CancellationToken? signal = null)
+    {
+        var tasks = slices.Select(slice => DownloadRangeFromS3Async(slice.offset, slice.length + slice.offset, signal));
+        return await Task.WhenAll(tasks);
+    }
+
+    public async Task<ArrayBuffer> FetchSlice(Slice slice, CancellationToken? signal = null)
+    {
+        return await this.DownloadRangeFromS3Async(slice.offset, slice.length + slice.offset, signal);
     }
 }
