2a1c4d3fe56b46991a624a9342f74de7f483bd43
oshawa-connection <jf13g13@gmail.com>
2026-06-21 11:25:48 +0100

    Remove dead test

--- GeotiffTests/MaskedReadingTests.cs
+++ GeotiffTests/MaskedReadingTests.cs
@@ -6,39 +6,6 @@ namespace GeotiffTests;
 [TestClass]
 public class MaskedReadingTests : GeoTiffTestBaseClass
 {
-    // [TestMethod]
-    // public async Task ExternalMaskFileReading()
-    // {
-    //     // TODO: would be nice to have this in the main code someplace.
-    //     string externalOverviewTifPath = Path.Combine(GetDataFolderPath(), "masked_image.tif");
-    //     var mskFilePath = externalOverviewTifPath + ".msk";
-    //     
-    //     if (File.Exists(mskFilePath) is false)
-    //     {
-    //         throw new FileNotFoundException($"No file .msk file found at {mskFilePath}");
-    //     }
-    //     
-    //     await using var mainStream = File.OpenRead(externalOverviewTifPath);
-    //     await using var mskFileStream = File.OpenRead(mskFilePath);
-    //     
-    //     var maskedMultiTiff = await MultiGeoTiff.FromStreams(mainStream, new[] { mskFileStream });
-    //     var maskedReader = await MaskedGeoTiffReader.FromExternalMaskGeoTiffAsync(maskedMultiTiff);
-    //     var rightReadResult = await maskedReader.ReadMaskedRasterAsync(new ImagePixelWindow() {MinRow = 0, MaxRow = 50, MinColumn = 25, MaxColumn = 50});
-    //
-    //     var rightSample = rightReadResult.GetSampleAt(0);
-    //     var rightPixelArray = rightSample.GetAsDoubleArray();
-    //     rightPixelArray.ShouldAllBe(d => d.IsMasked == true);
-    //     
-    //     
-    //     var leftReadResult = await maskedReader.ReadMaskedRasterAsync(new ImagePixelWindow() {MinRow = 0, MaxRow = 50, MinColumn = 0, MaxColumn = 24});
-    //
-    //     var leftSample = leftReadResult.GetSampleAt(0);
-    //     var leftPixelArray = leftSample.GetAsDoubleArray();
-    //     leftPixelArray.ShouldAllBe(d => d.IsMasked == false);
-    //
-    // }
-
-
     /// <summary>
     /// This is a tif where the left half is valid, and the right half is masked off.
     /// TODO: Might be good to create another, more explicit test where we read bit raster data. 
@@ -82,7 +49,17 @@ public class MaskedReadingTests : GeoTiffTestBaseClass
         var readResult = await image.ReadRasterMaskedAsync();
         var sample = readResult.GetSampleAt(0);
         var maskedDoubleArray = sample.GetAsMaskedDoubleArray();
-        maskedDoubleArray.ShouldAllBe(d => d.IsMasked == true);
+        foreach (var v in maskedDoubleArray)
+        {
+            if (v.Value == -9999)
+            {
+                v.IsMasked.ShouldBeTrue();
+            }
+            else
+            {
+                v.IsMasked.ShouldBeFalse();
+            }
+        }
     }
 
 
