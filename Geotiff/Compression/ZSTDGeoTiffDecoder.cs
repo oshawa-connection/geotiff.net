77c5bd25e4e84a01282943e690d70bc66822e923
oshawa-connection <jf13g13@gmail.com>
2026-08-12 12:16:17 +0200

    Add zstd compression support sync method

--- Geotiff/Compression/ZSTDGeoTiffDecoder.cs
+++ Geotiff/Compression/ZSTDGeoTiffDecoder.cs
@@ -6,20 +6,22 @@ namespace Geotiff.Compression;
 
 public class ZSTDGeoTiffDecoder : GeoTiffDecoder
 {
-    public override IEnumerable<int> codes => new[] { 50000,34887 };
+    public override IEnumerable<int> codes => new[] { 50000 };
     protected override async Task<byte[]> DecodeBlockAsync(byte[] buffer, GeoTiffImage image)
     {
         using var ms = new MemoryStream(buffer);
-        using var outputFileStream = new MemoryStream();
         await using var ds = new DecompressionStream(ms);
+        using var outputFileStream = new MemoryStream();
         await ds.CopyToAsync(outputFileStream);
-        
-        var outArray = outputFileStream.ToArray();
-        return outArray;
+        return outputFileStream.ToArray();
     }
 
     protected override byte[] DecodeBlock(byte[] buffer, GeoTiffImage image)
     {
-        throw new NotImplementedException();
+        using var ms = new MemoryStream(buffer);
+        using var ds = new DecompressionStream(ms);
+        using var outputFileStream = new MemoryStream();
+        ds.CopyTo(outputFileStream);
+        return outputFileStream.ToArray();
     }
 }
